a0cf58366304e751c68d1ca1f9dab99433835cdd
d0m G <dom@ginhac.com>
2022-12-02 14:51:13 +0100

    Update loop with final message

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main()
 {
     for (auto i=0; i<10; i++) {
-        std::cout << "Hello World: " << i << std::endl;
-        std::cout << "Bye!" << std::endl;
+        std::cout << "Hello World: " << i << std::endl; 
     }
+    std::cout << "Bye!" << std::endl;
     return 0;
 }
