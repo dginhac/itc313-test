faa3fa3caee93ada06ab67ac783a80cb5fa4dab7
d0m G <dom@ginhac.com>
2022-12-02 14:32:57 +0100

    Add several prints on console

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Bye!" << std::endl;
     return 0;
 }
