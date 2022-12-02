f148f9d98e3bd094f63699b620d4257595c4570d
d0m G <dom@ginhac.com>
2022-12-02 15:03:30 +0100

    Update  main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@ int main()
 {
     for (auto i=0; i<10; i++) {
         std::cout << "Hello World: " << i << std::endl; 
-    }
-    std::cout << "Bye!" << std::endl;
-    
+    }    
     Date d(1, 1, 2019);
     std::cout << d << std::endl;
     return 0;
