57befe6353157a5bb52b2ea49a100c79512c3373
d0m G <dominique.ginhac@u-bourgogne.fr>
2024-10-11 14:54:07 +0200

    add setters with date validation

--- date.h
+++ date.h
@@ -3,6 +3,8 @@ class Date {
         Date(int month, int day);
         int month() const;
         int day() const;
+        void updateMonth(int new_month);
+        void updateDay(int new_day);
     private:
         int _month;
         int _day;
