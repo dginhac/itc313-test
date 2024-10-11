6acffde27dde7044a92f75312d654032aca1d6eb
d0m G <dominique.ginhac@u-bourgogne.fr>
2024-10-11 14:35:56 +0200

    add test of valid date

--- main.cpp
+++ main.cpp
@@ -4,4 +4,6 @@
 int main() {
     Date today(10,11);
     std::cout << "today: " << today.month() << "/" << today.day() << std::endl;
+    Date a_day(15,11);
+    std::cout << "a_day: " << a_day.month() << "/" << a_day.day() << std::endl;
 }
